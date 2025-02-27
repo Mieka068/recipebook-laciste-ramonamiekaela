8995f8435d9ba973246ca0cf299106f32d27c5b8
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-02-27 18:51:57 +0800

    modified to add recipe_list and recipe_detail

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import recipe_list, recipe_detail
 
 app_name = "ledger"
 
 urlpatterns = [
-    path("", views.recipe_list, name='recipe_list'),
-    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    path("recipes/list/", recipe_list, name='recipe_list'),
+    path('recipe/<int:recipe_id>/', recipe_detail, name='recipe_detail'),
 ]
