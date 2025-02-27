a81e5fd5823ff5dba45a638aa7c341aeec57afe3
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-02-27 15:51:16 +0800

    Incorporated the recently created models

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,38 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-# Context data for recipes
-RECIPES = [
-    {
-        "name": "Recipe 1",
-        "ingredients": [
-            {"name": "tomato", "quantity": "3pcs"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "pork", "quantity": "1kg"},
-            {"name": "water", "quantity": "1L"},
-            {"name": "sinigang mix", "quantity": "1 packet"},
-        ],
-        "link": "/recipe/1"
-    },
-    {
-        "name": "Recipe 2",
-        "ingredients": [
-            {"name": "garlic", "quantity": "1 head"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "vinegar", "quantity": "1/2cup"},
-            {"name": "water", "quantity": "1 cup"},
-            {"name": "salt", "quantity": "1 tablespoon"},
-            {"name": "whole black peppers", "quantity": "1 tablespoon"},
-            {"name": "pork", "quantity": "1 kilo"},
-        ],
-        "link": "/recipe/2"
-    }
-]
-
-# View for listing recipes
 def recipe_list(request):
-    return render(request, "recipe_list.html", {"recipes": RECIPES})
+    recipes = Recipe.objects.all()
+    return render(request, "recipe_list.html", {"recipes":recipes})
 
-# View for individual recipe details
 def recipe_detail(request, recipe_id):
-    recipe = next((r for r in RECIPES if r["link"] == f"/recipe/{recipe_id}"), None)
-    return render(request, "recipe_detail.html", {"recipe": recipe})
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, "recipe_detail.html", {"recipe: recipe"})
