78819b78644cfca8b1383f96b482e57f33cbcce5
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-02-27 15:59:54 +0800

    Modified urls.py to contain only two paths

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "ledger"
 
 urlpatterns = [
-    path('', views.recipe_list, name='recipe_list'),  # This makes / point to recipes list
-    path('recipes/list/', views.recipe_list, name='recipe_list'),
+    path("", views.recipe_list, name='recipe_list'),
     path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
 ]
