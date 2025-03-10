1ca7c58bb5f18fcc67fc15cf0ad2c677639f60c0
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-10 18:25:04 +0800

    Modified to make it login_required

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
-
 
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    return render(request, "recipe_list.html", {"recipes":recipes})
 
+    return render(request, "ledger/recipe_list.html", {"recipes":recipes})
+
+@login_required
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
-    return render(request, "recipe_detail.html", {"recipe": recipe})
+    
+    return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
