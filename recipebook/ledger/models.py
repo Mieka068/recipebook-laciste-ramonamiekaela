3a847aca804f275e8865f426a0c9c158e671bdd2
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-10 14:59:23 +0800

    Added Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Ingredient(models.Model):
@@ -7,6 +8,7 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
@@ -17,6 +19,7 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.id)])
 
+
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
@@ -24,3 +27,9 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient.name} for {self.recipe.name}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio=models.TextField()
