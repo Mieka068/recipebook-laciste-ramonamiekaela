ee896028b22f56ad812f5bbfa797c44795537d23
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-10 18:20:06 +0800

    Added for Profile Model Admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
@@ -8,6 +10,19 @@ class RecipeIngredientInline(admin.TabularInline):
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
+admin.site.register(Profile)
 
