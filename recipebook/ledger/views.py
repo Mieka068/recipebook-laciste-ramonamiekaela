c60bf5ceac3bf733e092b28901134ac9e6d9eae5
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-24 17:37:52 +0800

    Added classes for AddRecipe and AddImage views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
@@ -12,3 +16,29 @@ def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     
     return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
+
+
+class AddRecipeView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = "ledger/add_recipe.html"  
+    success_url = reverse_lazy("ledger:recipe_list")  
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  
+        return super().form_valid(form)
+
+
+class AddImageView(CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = "ledger/add_image.html"
+
+    def form_valid(self, form):
+        form.instance.recipe = get_object_or_404(Recipe, id=self.kwargs["recipe_id"])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("ledger:recipe_detail", kwargs={"recipe_id": self.kwargs["recipe_id"]})
+    
+    
