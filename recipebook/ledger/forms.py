97de160f2afe2ba805b2d34cea3ff3354490a584
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-24 23:05:47 +0800

    made updates

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from .models import Recipe, RecipeImage
-
+from django.forms import inlineformset_factory
+from .models import Recipe, RecipeImage, RecipeIngredient, Ingredient
 
 class RecipeForm(forms.ModelForm):
     class Meta:
@@ -8,7 +8,34 @@ class RecipeForm(forms.ModelForm):
         fields = ["name"]
 
 
-class RecipeImageForm(forms.ModelForm):
+class RecipeImageForm(forms.ModelForm):  # ✅ Make sure this exists!
     class Meta:
         model = RecipeImage
         fields = ["image", "description"]
+
+
+class RecipeIngredientForm(forms.ModelForm):
+    new_ingredient = forms.CharField(
+        required=False, 
+        label="Or create a new ingredient"
+    )  # ✅ Allows adding a new ingredient
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ["ingredient", "quantity"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        ingredient = cleaned_data.get("ingredient")
+        new_ingredient = cleaned_data.get("new_ingredient")
+
+        if ingredient and new_ingredient:
+            raise forms.ValidationError("Choose an existing ingredient OR enter a new one, not both.")
+
+        return cleaned_data
+
+RecipeIngredientFormSet = inlineformset_factory(
+    Recipe, RecipeIngredient, form=RecipeIngredientForm, extra=1, can_delete=True
+)
+
+# working version
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,51 +1,52 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.forms import inlineformset_factory
-from .models import Recipe, RecipeImage, RecipeIngredient
-from .forms import RecipeForm, RecipeImageForm, RecipeIngredientFormSet
+from .models import Recipe, RecipeIngredient, Ingredient, RecipeImage
+from .forms import RecipeForm, RecipeIngredientFormSet, RecipeImageForm
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
-
-    return render(request, "ledger/recipe_list.html", {"recipes":recipes})
+    return render(request, "ledger/recipe_list.html", {"recipes": recipes})
 
 @login_required
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
-    
     return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
-
 
 class AddRecipeView(LoginRequiredMixin, CreateView):
     model = Recipe
     form_class = RecipeForm
-    template_name = "ledger/add_recipe.html"  
+    template_name = "ledger/add_recipe.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.POST:
-            context["formset"] = RecipeIngredientFormSet(self.request.POST)
+            context["ingredient_formset"] = RecipeIngredientFormSet(self.request.POST)
         else:
-            context["formset"] = RecipeIngredientFormSet()
+            context["ingredient_formset"] = RecipeIngredientFormSet()
         return context
 
     def form_valid(self, form):
-        form.instance.author = self.request.user  
-        self.object = form.save()  
+        context = self.get_context_data()
+        ingredient_formset = context["ingredient_formset"]
+        form.instance.author = self.request.user  # ✅ Assign logged-in user as the author
 
-        # Process ingredient formset
-        formset = RecipeIngredientFormSet(self.request.POST, instance=self.object)
-        if formset.is_valid():
-            formset.save()
+        if form.is_valid() and ingredient_formset.is_valid():
+            self.object = form.save()
+            ingredient_formset.instance = self.object
+            
+            for ingredient_form in ingredient_formset:
+                if ingredient_form.cleaned_data.get("new_ingredient"):
+                    new_ingredient_name = ingredient_form.cleaned_data["new_ingredient"]
+                    new_ingredient, created = Ingredient.objects.get_or_create(name=new_ingredient_name)
+                    ingredient_form.instance.ingredient = new_ingredient  # ✅ Assign new ingredient
 
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy("ledger:recipe_detail", kwargs={"recipe_id": self.object.id})
-
+            ingredient_formset.save()
+            return redirect("ledger:recipe_list")
+        else:
+            return self.form_invalid(form)
 
 
 class AddImageView(CreateView):
@@ -64,5 +65,3 @@ class AddImageView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy("ledger:recipe_detail", kwargs={"recipe_id": self.kwargs["recipe_id"]})
-    
-    
