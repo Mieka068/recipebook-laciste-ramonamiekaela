3bf69bc4277c8678e702051371d02364c5d31844
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-24 19:41:33 +0800

    updated image and description fields

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -43,8 +43,8 @@ class Profile(models.Model):
 
 class RecipeImage(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="recipe_images/")
-    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="recipe_images/", blank=False, null=False)
+    description = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return f"Image for {self.recipe.name}"
