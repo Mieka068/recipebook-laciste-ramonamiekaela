154b8a7544212baaf84ceb8770838931e993ea23
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-24 17:37:20 +0800

    Added paths to AddRecipe and AddImage views

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail
+from .views import recipe_list, recipe_detail, AddRecipeView, AddImageView
 
 
 app_name = "ledger"
@@ -7,4 +7,6 @@ app_name = "ledger"
 urlpatterns = [
     path("recipes/list/", recipe_list, name="recipe_list"),
     path("recipe/<int:recipe_id>/", recipe_detail, name="recipe_detail"),
+    path("recipe/add/", AddRecipeView.as_view(), name="add_recipe"),
+    path("recipe/<int:recipe_id>/add_image/", AddImageView.as_view(), name="add_image"),
 ]
